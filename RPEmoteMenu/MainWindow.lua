1d6af7fa8db89e60057374a481dbd19216eabad8
bblackmoor <33701300+bblackmoor@users.noreply.github.com>
2026-09-18 18:00:09 -0400

    Deemphasize emote edit icons until hover

--- RPEmoteMenu/MainWindow.lua
+++ RPEmoteMenu/MainWindow.lua
@@ -963,6 +963,9 @@ end
 
 local function SetEmoteHovered(button, isHovered)
     button.isHovered = isHovered
+    if button.EditButton then
+        button.EditButton:SetAlpha(isHovered and 1 or 0.25)
+    end
     ApplyEmoteHoverHighlight(button)
 end
 
@@ -1038,6 +1041,7 @@ local function GetContainerButton()
     button.EditButton:SetFrameLevel(button:GetFrameLevel() + 2)
     button.EditButton:RegisterForClicks("LeftButtonUp")
     button.EditButton:SetNormalTexture("Interface\\Buttons\\UI-OptionsButton")
+    button.EditButton:SetAlpha(0.25)
     button.EditButton:SetHighlightTexture(
         "Interface\\Buttons\\ButtonHilight-Square",
         "ADD"
